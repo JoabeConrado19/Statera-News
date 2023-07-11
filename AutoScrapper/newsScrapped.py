87e6714bb0f3193ec57cac34ca3d825f792fb42b
JoabeConrado19 <joabe.conrado19@gmail.com>
2023-07-11 17:36:51 -0400

    feat: news scrapper

--- AutoScrapper/newsScrapped.py
+++ AutoScrapper/newsScrapped.py
@@ -2,7 +2,7 @@ from django.conf import settings
 import requests
 
 def schedule_api():
-    response = requests.get("https://nerdnucleusapidjango.onrender.com/api/news/startscrappy/")
+    response = requests.get("https://statera-news.onrender.com/api/news/startscrappy/")
 
 
 
